13028a13370b1f92e144f8903ffc40cee18fcce9
KeuryLendof <lendofkeury@gmail.com>
2022-02-22 22:16:03 -0400

    probando micro

--- app.js
+++ app.js
@@ -52,5 +52,23 @@ btnStop.addEventListener('click', () => {
     recognition.abort()
 })
 
-    
 
+// let rec;
+
+// if(!("webkitSpeechRecognition" in window)){
+//     alert("No funciona")
+// }else{
+//     rec = new webkitSpeechRecognition();
+//     rec.lang = "es-AR";
+//     rec.continuous = true;
+//     rec.interim = true;
+//     rec.addEventListener("result", iniciar)
+// }
+
+// function iniciar(event){
+//     for(i = event.resultIndex; i < event.results.length; i++){
+//         document.getElementById("texto").innerHTML = event.results[i][0].transcript;
+//     }
+// }
+    
+// rec.start();
